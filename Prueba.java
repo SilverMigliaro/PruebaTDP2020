bfe0265b7f98fec2e04e7ec58d4b4103b8fccaed
SilverMigliaro <silve.migliaro@hotmail.com>
2020-09-18 19:45:08 -0300

    inicial

---
+++ Prueba.java
@@ -0,0 +1,11 @@
+public class Prueba{
+	
+
+	public static void main(String [] agrs){
+
+		Persona a = new Persona("Nicolas");
+
+		System.out.println("El nombre de la persona es : "+a.getName());
+
+	}
+}
