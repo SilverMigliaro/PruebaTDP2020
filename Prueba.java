25fe9b877e86f425301fb7f4935645bc765a36e4
SilverMigliaro <silve.migliaro@hotmail.com>
2020-09-20 03:16:37 -0300

    Cuarto

--- Prueba.java
+++ Prueba.java
@@ -3,7 +3,7 @@ public class Prueba{
 
 	public static void main(String [] agrs){
 
-		Persona a = new Persona("Nicolas");
+		Persona a = new Persona("Matias");
 
 		System.out.println("El nombre de la persona es : "+a.getName());
 
